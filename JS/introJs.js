7092e9c8d8b6ffc46a0a83404fb193a3311e4fb5
josuepl <josue.perez.lucero@gmail.com>
2022-03-30 15:42:32 -0600

    Update introJs.js

--- JS/introJs.js
+++ JS/introJs.js
@@ -11,7 +11,19 @@ console.log(variableConst)
 function prueba(){
     console.log(variableConst);
     let texto1 = "Hola Mundo";
-    let encabezado = "<header>"+ texto1 +"</header>"
+    let encabezado = "<header>"+ texto1 +"</header>";
     let texto2 = "APPWeb";
-    document.write(encabezado +" Bienvenidos a "+texto2)
+    let parrafo = `<p>Este es el curso de ${texto2} espero que funcione ...</p>`;
+    let tarjeta = 
+    `<article class="tarjeta">
+    <header class="myHeader"><h1>León</h1></header>
+    <div class="myContenido">
+        <h2>Descripción</h2>
+    <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, architecto minus? Est nemo cum provident ipsam molestiae explicabo eum unde, ut nesciunt minima voluptas adipisci atque. Inventore fugit reprehenderit modi!</p>
+    </div>
+    <div class="myImg">
+       <img  src="https://cumbrepuebloscop20.org/wp-content/uploads/2018/09/Le%C3%B3n-2.jpg" alt="Leon" srcset="https://cumbrepuebloscop20.org/wp-content/uploads/2018/09/Le%C3%B3n-2.jpg">
+    </div>`;
+
+    document.write(encabezado +" Bienvenidos a "+parrafo + tarjeta)
 }
