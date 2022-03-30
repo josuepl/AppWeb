d52a4503d4bddd1955a40191d587cab19c7477ba
josuepl <josue.perez.lucero@gmail.com>
2022-03-30 15:34:24 -0600

    Update

--- JS/introJs.js
+++ JS/introJs.js
@@ -1,6 +1,17 @@
-var variable
+var variable;
+let variableLet;
+const variableConst = 9.81;
+/*
 alert("la variable contiene: "+ variable)
 variable = "TEXTO"
 alert("la variable contiene: "+ variable)
 variable = true
-alert("la variable contiene: "+ variable)
+alert("la variable contiene: "+ variable)*/
+console.log(variableConst)
+function prueba(){
+    console.log(variableConst);
+    let texto1 = "Hola Mundo";
+    let encabezado = "<header>"+ texto1 +"</header>"
+    let texto2 = "APPWeb";
+    document.write(encabezado +" Bienvenidos a "+texto2)
+}
